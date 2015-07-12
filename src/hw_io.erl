9b18b54417ea78baabd9d4ca6ae606a016e2389c
RaspberryPi dev box <ugrow@cascadiamicrogrow.com>
2015-07-12 02:38:43 +0000

    added adafruit lib and made rudimentary call

--- src/hw_io.erl
+++ src/hw_io.erl
@@ -1,10 +1,12 @@
 -module(hw_io).
 
--export([i2c_read/2, i2c_write/3]).
+-export([sensor_read/2, i2c_write/3]).
 -include("hw_io.hrl").
 
-i2c_read(State, Register) ->
-	python:call(State#hw_state.pyPID, hw_io, i2c_read, [State#hw_state.network, State#hw_state.port, Register]).
+sensor_read(State, Register) ->
+	%Volt = python:call(State#hw_state.pyPID, ads1x15_ex_singleended, read_volts, [State#hw_state.network, State#hw_state.port, Register]),
+	Volt = python:call(State#hw_state.pyPID, ads1x15_ex_singleended, read_volts, []),
+	Volt/1000.
 
 i2c_write(State, Register, Data) ->
 	python:call(State#hw_state.pyPID, hw_io, i2c_write, [State#hw_state.network, State#hw_state.port, Register, Data]).
