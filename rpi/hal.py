843382c01c85a7c876fb3605d48d456189b058c0
RaspberryPi dev box <ugrow@cascadiamicrogrow.com>
2015-07-13 06:15:39 +0000

    added temp/humidity sensor read functions and sensor library. added relay close function.

--- rpi/hal.py
+++ rpi/hal.py
@@ -2,6 +2,8 @@
 
 from ads1x15_ex_singleended import read_volts
 import pigpio
+from  HTU21DF import read_temperature
+from  HTU21DF import read_humidity
 
 def current_read(channel):
     return read_volts(channel)
@@ -19,3 +21,21 @@ def relay_open(channel):
         return 0
     else:
         return -1  
+
+def relay_close(channel):
+    pi = pigpio.pi()
+    relay_pins[8] = [7,0,0,0,0,0,0,0]
+    if pi.connected:
+        pi.set_mode(relay_pins[channel], pi.OUTPUT)
+        pi.write(relay_pins[channel], pi.LOW)
+        pi.stop()
+        return 0
+    else:
+        return -1  
+
+def temp_read(port):
+	return read_temperature
+
+def hum_read(port):
+	return read_humidity
+
